c690ab11ae4360cc21c822eca295353cf96f9a4d
DoganHaluk <ha_look@yahoo.com>
2022-05-11 16:28:05 +0200

    bpost address api

--- src/app/widget/widget.component.ts
+++ src/app/widget/widget.component.ts
@@ -4,7 +4,7 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-widget',
   templateUrl: './widget.component.html',
-  styleUrls: ['./widget.component.css']
+  styleUrls: ['./widget.component.scss']
 })
 export class WidgetComponent implements OnInit {
 
